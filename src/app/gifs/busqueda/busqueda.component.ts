54ebb40787239d5b353c11a30fd5f14495eafbfb
Edison Perdomo <lalvarado@achs.cl>
2021-12-15 11:21:10 -0400

    feature: view child search field

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,18 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styleUrls: ['./busqueda.component.css']
+})
+export class BusquedaComponent  {
+
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+
+  buscar(){
+    const valor = this.txtBuscar.nativeElement.value;
+
+    this.txtBuscar.nativeElement.value = '';
+  }
+
+}
