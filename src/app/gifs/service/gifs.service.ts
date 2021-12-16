0e0a58846f644b886843ca1fe6a044a407186e1e
Edison Perdomo <lalvarado@achs.cl>
2021-12-16 16:59:06 -0400

    feat: http gifs api

--- src/app/gifs/service/gifs.service.ts
+++ src/app/gifs/service/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,14 +7,31 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _historial: string[] = [];
+  private apiKey: string = 'xK2zA9Iv8BX57cW8zIci68xNJ15wWS9I';
 
-  constructor() { }
+  //TODO cambiar any por su tipo
+  public resultados: any[] = [];
+
+  constructor(private http: HttpClient) { }
 
   get historial(){
+    
     return [...this._historial];
   }
 
   buscarGifs (query: string) {
+
+    query = query.trim().toLocaleLowerCase();
+
+    if(!this._historial.includes(query)){
       this._historial.unshift(query);
+      this._historial = this._historial.splice(0,10);
+    }
+  
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=xK2zA9Iv8BX57cW8zIci68xNJ15wWS9I&q=${query}&limit=10`)
+      .subscribe((resp:any) => {
+        console.log(resp.data);
+         this.resultados= resp.data; 
+      });
   }
 }
